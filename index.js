bd84d3f249f6c5ccc35e6af4d0a8ecad8c3f27f8
Rafaeltech2025 <rafael.campos34@fatec.sp.gov.br>
2025-05-09 19:30:16 +0000

    Finalizando Atvidade 09_05

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello Word");
+
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+res.send('Hello World!');
+
+});
+
+
+app.listen(3000, () => {
+
+console.log('Servidor rodando em http://localhost:3000');
+
+});
